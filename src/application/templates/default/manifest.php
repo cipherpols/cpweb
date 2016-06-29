3aa3f6b7ac88c49f07770eb7ed960869fa5a24e2
Tuan Duong <bacduong@gmail.com>
2016-06-29 23:16:38 +0900

    Initial framework and resources

---
+++ src/application/templates/default/manifest.php
@@ -0,0 +1,28 @@
+<?php
+return array(
+    'title' => 'CipherPols Website',
+    'description' => 'Official website of CipherPols',
+    'date' => 'July 2016',
+    'author' => 'CipherPols',
+    'layout' => array(
+        'main' => array(
+            'css' => array(
+                // 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
+                // 'https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',
+            ),
+            'js' => array(
+                // '/assets/js/libs/jquery-2.1.4.min.js',
+                // 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js',
+            ),
+        ),
+        'login' => array(
+            'js' => array(
+                // '/assets/js/libs/jquery-2.1.4.min.js',
+                // 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js',
+            ),
+            'css' => array(
+                // 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
+            ),
+        ),
+    ),
+);
