ad9dff9d3e2131b49c7e624efb8bacc13c6ed5ff
Tuan Duong <bacduong@gmail.com>
2016-07-01 00:29:48 +0900

    Organize the layout

---
+++ src/library/Cp/Controller.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * File Controller.php
+ *
+ * @author Tuan Duong <duongthaso@gmail.com>
+ * @package CipherPols
+ * @version 1.0
+ */
+namespace Cp;
+
+/**
+ * Class Controller
+ * @package Cp
+ */
+class Controller extends \Cze\Controller
+{
+
+}
